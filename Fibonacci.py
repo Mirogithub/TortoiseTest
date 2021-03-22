d6afb49ea5031303cbdb46fd7376eab15a198691
Myroslav <mailtomiroslav@gmail.com>
2021-03-23 00:24:14 +0200

    Adding Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,35 @@
+def print_odds(reslist):
+
+    countodds = []
+    for i in reslist:
+        if i % 2 != 0:
+            countodds.append(i)
+    countodds= len(countodds)
+    countevens= len(reslist) - countodds
+    print(f'In the Fibonacci list you have {countodds} odds and {countevens} even values')
+
+
+
+def fibonacci(a):
+
+    if type(a) != int or a < 1:
+        return print("Please enter correct value!")
+
+    for b in range(a):
+
+        if (b == 0):
+            prev1 = prev2 = 1;
+            reslist = [prev1];
+        else:
+            res = prev1 + prev2
+            prev1 = prev2
+            prev2 = res
+
+            if res > a:
+                break
+            reslist.append(res)
+    # print(reslist)
+    print_odds(reslist)
+
+
+fibonacci(4000000)
